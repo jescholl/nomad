85bc562f436f0569e1e21319c9440499f19f74fe
Jason Scholl <jason.e.scholl@gmail.com>
2021-01-24 14:48:51 -0800

    Add app policy script

---
+++ app_policy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+app_policy() {
+  app_name=$1
+
+  cat | vault policy write "$app_name" - <<EOT
+# key/value secrets
+path "secret/app/$app_name/*"
+{
+  capabilities = ["read","list"]
+}
+
+path "secret/global/*"
+{
+  capabilities = ["read","list"]
+}
+EOT
+}
+
+app_policy plex
+app_policy observium
+app_policy pihole
